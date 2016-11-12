1f7054fa0e3285905b14bf5d90ef470ba3f44143
Daz <daz.lee1987@gmail.com>
2016-11-12 22:27:07 +0800

    add bet api

--- server/routes/api/user.js
+++ server/routes/api/user.js
@@ -15,11 +15,24 @@ router.get("/me", (req, res) => {
         res.end();
     });
 });
-
 router.post("/me/gain", (req, res) => {
     const woney = req.body.woney;
     UserStore.updateUser("58271c0c85ff2f09d86408ce", {
         woney
+    })
+    .then((user) => {
+        res.json(user);
+    })
+    .catch((error) => {
+        res.status(400).send(error);
+        res.end();
+    });
+});
+router.post("/me/bet", (req, res) => {
+    const woney = req.body.woney,
+          bets = req.body.bets;
+    UserStore.updateUser("58271c0c85ff2f09d86408ce", {
+        woney, bets
     })
     .then((user) => {
         res.json(user);
